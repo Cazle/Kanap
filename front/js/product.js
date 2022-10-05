f56c306fd5d9f123646ee9e63a840389873c09fb
Cazle <kyllianglt@gmail.com>
2022-10-05 12:09:56 +0200

    Ajout du titre des canapés dans la page produit et lancement du server

---
+++ front/js/product.js
@@ -0,0 +1,21 @@
+// on va récupérer l'url de notre page
+const urlId = window.location.search;
+// on utilise URLsearchParams pour récupérer l'id
+const UrlsearchParams = new URLSearchParams(urlId);
+// on récupère la valeur de l'id
+const id = UrlsearchParams.get('id');
+
+// on fetch par rapport à l'id
+fetch(`http://localhost:3000/api/products/${id}`)
+    // on récupére et interprète le résultat au format JSON
+    .then (data => data.json())
+    // on alimente la vue
+    .then (productId => {
+        document.querySelector('.item__img').innerHTML = `<img src="${productId.imageUrl}" alt="${productId.altTxt}">`;
+        document.getElementById('title').innerHTML = `<h1 id="title">${productId.name}</h1>`
+    })
+    // on affiche un message d'erreur en cas de problème
+    .catch( function(err) {
+        let error = document.querySelector("main");
+        error.innerHTML = `<h2 style="color:red;">Une erreur est survenue !</h2>`
+    })
---
+++ front/js/script.js
@@ -0,0 +1,28 @@
+/*
+Insertion produits page d'accueil
+*/
+
+// créer une requête API fetch sous forme de promesse
+fetch("http://localhost:3000/api/products")
+    // récupère les données et on les interprète au format JSON
+    .then(data => data.json())
+    .then(listOfProduct => {
+        //traiter les données dans l'ordre d'arrivée grâce à une boucle for
+        for (product of listOfProduct) {
+            document.getElementById("items").innerHTML += `
+            <a href="./product.html?id=${product._id}">
+                <article>
+                <img src="${product.imageUrl}" alt="${product.altTxt}">
+                <h3 class="productName">${product.name}</h3>
+                <p class="productDescription">${product.description}</p>
+                </article>
+            </a> 
+            `
+        }
+    })
+    
+    // on affiche un message d'erreur en cas de problème
+    .catch( function(err) {
+        let error = document.querySelector("main");
+        error.innerHTML = `<h2 style="color:red;">Une erreur est survenue !</h2>`
+    })
