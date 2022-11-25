37102ac23d28420a5f2981913271efa816295534
Cazle <kyllianglt@gmail.com>
2022-11-25 21:09:35 +0100

    ajout nouveau code

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,66 +1,81 @@
 const productInLocalStorage = JSON.parse(localStorage.getItem("cart"));
 
 const container = document.getElementById("cart__items")
+const quantity = document.getElementById('totalQuantity');
+const price = document.getElementById('totalPrice');
+
+let globalQuantity = 0;
+let globalPrice = 0;
 
 if (productInLocalStorage === null) {
     container.innerHTML = `<section id="cart__items"><h2>Le panier est vide<h2></section>`
 }
 
-
-productInLocalStorage.forEach((color, i)=> {
-    const article =`
-        <article class="cart__item" data-id="${productInLocalStorage[i]._id}" data-color="${productInLocalStorage[i].colors}">
-                <div class="cart__item__img">
-                  <img src="${productInLocalStorage[i].imageUrl}" alt="${productInLocalStorage[i].altTxt}">
+const generateCard = (product) => {
+    return `
+    <article class="cart__item" data-id="${product._id}" data-color="${product.colors}">
+        <div class="cart__item__img">
+            <img src="${product.imageUrl}" alt="${product.altTxt}">
+        </div>
+    
+        <div class="cart__item__content">
+    
+            <div class="cart__item__content__description">
+                <h2>${product.name}</h2>
+                <p>${product.colors}</p>
+                <p>${product.price}</p>
+            </div>
+    
+            <div class="cart__item__content__settings">
+    
+                <div class="cart__item__content__settings__quantity">
+                    <p>Qté : </p>
+                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100"
+                           value="${product.quantity}">
                 </div>
-                <div class="cart__item__content">
-                  <div class="cart__item__content__description">
-                    <h2>${productInLocalStorage[i].name}</h2>
-                    <p>${productInLocalStorage[i].colors}</p>
-                   <p>${productInLocalStorage[i].price}</p>
-                  </div>
-                  <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                      <p>Qté : </p>
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${productInLocalStorage[i].quantity}">
-                    </div>
-                    <div class="cart__item__content__settings__delete">
-                      <p class="deleteItem">Supprimer</p>
-                    </div>
+    
+                <div class="cart__item__content__settings__delete">
+                    <p class="deleteItem">Supprimer</p>
                 </div>
-              </article>
-      `;
+    
+            </div>
+    
+        </div>
+    
+    </article>
+    `
+};
 
-    container.insertAdjacentHTML("afterbegin", article);
+productInLocalStorage.forEach((product) => {
+    const article = generateCard(product);
 
+    globalQuantity = parseInt(globalQuantity) + parseInt(product.quantity);
+    quantity.innerHTML = parseInt(globalQuantity);
+
+    globalPrice = parseInt(globalPrice) + (parseInt(product.price) * parseInt(product.quantity));
+    price.innerHTML = parseInt(globalPrice);
+
+    container.insertAdjacentHTML("beforebegin", article);
+});
+
+const btns = document.querySelectorAll('.cart__item__content__settings__delete');
+const quantities = document.querySelectorAll('.itemQuantity');
+
+quantities.forEach((quantity, i) => {
+    quantity.addEventListener('change', () => {
+        productInLocalStorage[i].quantity = parseInt(quantity.value);
+        window.localStorage.setItem('cart', JSON.stringify(productInLocalStorage));
+        window.location.reload();
+    })
 })
 
-function removeItem(){
-
-  const deleteItem = document.querySelectorAll("deleteItem")
-
-
-  const getId = productInLocalStorage[i]._id;
-  const getColors =productInLocalStorage[i].colors;
-
-
-  let findCart = cart.find(product => product.getId === getId && product.getColors === getColors)
-  let filterCart = cart.filter(filterCart !== findCart)
-
-
-  deleteItem.forEach(i = 0, i < deleteItem.length, i++);{
-
-     deleteItem.addEventListener("click", (i) => {
-      cart = productInLocalStorage();
-      cart.splice(i, 1)
-      cart.remove();
-      saveCart(JSON.stringify('cart'));
-
+btns.forEach((btn, i) => {
+    btn.addEventListener('click', (event) => {
+        productInLocalStorage.splice(i, 1);
+        window.localStorage.setItem('cart', JSON.stringify(productInLocalStorage));
+        window.location.reload();
     })
-
-  }
-  removeItem();
-}
+});
 
 
 const Form = {
